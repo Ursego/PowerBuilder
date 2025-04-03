bc16b5a4468eafb0a542d5c0347c1ab18705e278
Ursego <90226705+Ursego@users.noreply.github.com>
2025-04-02 20:39:51 -0400

    Add files via upload

--- Overloading global functions (undocumented functionality).cs
+++ Overloading global functions (undocumented functionality).cs
@@ -2,18 +2,31 @@
 
 // "you cannot overload global functions."
 
-// It's true that we cannot overload global functions in the Function Painter.
-// But there is a secret: we can do that by editing code manually!
+// It's true regarding the Function Painter. But there is a secret: we can overload global functions by editing their code manually!
 // The steps:
 // 1. Create a normal, not-overloaded global function, save and close it.
 // 2. Re-open in with "Edit Source".
 // 3. Add the needed overloaded versions manually. For that, copy-paste the existing function as many times as neeedd, change the signatures and customize the code. Save.
 
 // When you later reopen it in the usual way, there’s no indication that it’s overloaded — you only see one version.  
-// Note that the version you see is the last overload in the source code.  
-// So, the explanatory header comment must be added to the last overload.  
-// That comment should clearly explain what the hell is going on here, using a fragment like:
+// Note that you see the overload which goes last in the source code.  
+// So, the explanatory header comment must be added to it.  
+// That comment should clearly explain what is going on here, using a fragment like:
 
 // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 // This function is overloaded. To see all the versions, open it in the "Edit Source" mode.
 // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// I’ve explained how to overload global functions, but only for cases where you need to override existing ones.
+// Avoid creating new global functions. Instead, define or modify classes, where function overloading is straightforward and all versions are immediately visible.
+
+// However, this repository describes three global functions I created from scratch, each with overloads:
+
+// * IfNull() - https://github.com/Ursego/PowerBuilder/blob/main/IfNull().cs
+// * iif() - https://github.com/Ursego/PowerBuilder/blob/main/iif().cs
+// * iin() - https://github.com/Ursego/PowerBuilder/blob/main/iin().cs
+
+// This was done intentionally because these functions are purely technical (not business-related) and don't belong to particular areas of functionality.
+// They recreate features that should have been implemented in PowerBuilder using keywords or operators.  
+// I use these functions so often in my code that I almost see them as part of the PowerScript language itself.  
+// If I had to prefix them with an object or class name every time, it would make my code unnecessarily verbose.
