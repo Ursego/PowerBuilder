febe906bd1ada0ee4b6fffe837a5d9a78022c23c
Ursego <90226705+Ursego@users.noreply.github.com>
2025-04-03 09:46:13 -0400

    Add files via upload

---
+++ Don't implement functionality in built-in events.cs
@@ -0,0 +1,70 @@
+// ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// If the functionality is longer than a few lines then don't implement it in system (built-in) events (such as ButtonClicked, ItemChanged etc.).
+// Instead, extract the logic into brand new well-named functions and call these functions from within the events.
+// ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// If a system event's script grows from just a few lines and starts to include its own logic, it effectively becomes "implementing functionality".
+// At that point, the logic should be extracted into a dedicated function (or dedicated functions).
+
+// You should divide your program into short methods, each performing a single, well-defined task (“separation of concerns”).  
+// So, if a built-in event needs to handle different logical tasks, implementing each task in its own function aligns with best coding practices.
+
+// In fact, some built-in events are really just traffic hubs for routing program flow to the appropriate functions using a choose case structure.
+
+// Here is an example for ButtonClicked (AcceptText() is omitted for simplicity):
+
+choose case dwo.name
+case "cb_calc_bonus"
+    this.uf_calc_bonus(row)
+case "cb_print_summary"
+    this.uf_print_summary(row)
+end choose
+
+// This approach makes it easier to focus on what happens when the user clicks one particular button, without being distracted by unrelated processing in the same script.
+
+// Also, consider this: one day, the event's functionality might need to be called from somewhere else.  
+// You should avoid calling built-in GUI events directly — unless you're using 'call super' from a descendant.  
+// On the other hand, calling a well-named function is simple and elegant.  
+// It also gives developers the flexibility to add arguments in the future to pass in the necessary data — something that’s not possible with built-in events.
+
+// @@@ ItemChanged
+
+// Sometimes, the processing of a field in ItemChanged includes a few short actions unrelated to each other.
+// Creating a function for each such action would be an overkill.
+// So, I extract the fragment for that field into one function, and name it using the pattern uf_<built-in event>__<column name> (note the two underscores).
+// For example: uf_itemchanged__birth_date, uf_itemchanged__country_id.
+
+// Each such a function must return the same values which ItemChanged returns:
+// 0 - Accept the new value and allow the focus to change (the default)
+// 1 - Reject the new value and don't allow focus to change; fire ItemError 
+// 2 - Restore the original value without firing ItemError and allow the focus to change
+
+// If the ItemChanged event is doing nothing else besides calling such functions, the script is extremely simple:
+
+choose case dwo.name
+case "birth_date"
+    return this.uf_itemchanged__birth_date(row)
+case "country_id"
+    return this.uf_itemchanged__country_id(row)
+end choose
+
+return 0
+
+// If there is something else to do, then use this pattern:
+
+int li_rc = 0
+
+choose case dwo.name
+case "birth_date"
+    li_rc = this.uf_itemchanged__birth_date(row)
+case "country_id"
+    li_rc = this.uf_itemchanged__country_id(row)
+end choose
+if li_rc <> 0 then return li_rc
+
+// ...do something else...
+
+return 0
+
+// It’s perfectly reasonable to end up with many uf_itemchanged__XXX functions - this happened to me a lot of times.
+
--- iin().cs
+++ iin().cs
@@ -4,34 +4,43 @@
 
 // Writing Oracle PL/SQL code, I always enjoy the ability to use the SQL's IN clause in a procedural environment:
 
-if v_day in ('SATURDAY', 'SUNDAY') then
-   -- do something
+if i_day in ('SATURDAY', 'SUNDAY') then
+   v_is_weekend := TRUE;
 end if;
 
-// Of course, we can mimic this behavior in PowerScript using a CHOOSE CASE construction — the only cost is one additional line of code:
+// Of course, we can mimic this in PowerScript using a CHOOSE CASE construction — the only cost is one additional line of code:
 
-choose case ls_day
+choose case as_day
 case 'SATURDAY', 'SUNDAY'
-   // do something
+   lb_weekend = TRUE
 end choose
 
-// Unfortunately, it's not possible to use CHOOSE CASE to assign the result of a comparison to a Boolean variable. To achieve this, we need to use a Boolean expression with OR:
+// Now, see this PL/SQL code - we assign the result of a comparison to a Boolean variable directly:
 
-lb_weekend = (ls_day = 'SATURDAY' or ls_day = 'SUNDAY')
+v_is_weekend BOOLEAN := (i_day IN ('SATURDAY', 'SUNDAY'));
 
-// That solution isn't very elegant — the same variable (ls_day) appears multiple times in the code. And what if you need to compare a variable against 30 values instead of just 2?
+// Unfortunately, we cannot do that in PowerScript since CHOOSE CASE doesn't return a value (as the 'switch' statements does in many language).
+// To achieve the same, we need a Boolean expression with OR:
 
-// Let me introduce the iin() function ("i"nternal "in"). It checks whether the value passed as the first argument exists in the array provided as the second argument:
+lb_weekend = (as_day = 'SATURDAY' or as_day = 'SUNDAY')
 
-lb_weekend = iin(ls_day, {'SATURDAY', 'SUNDAY'})
+// That solution isn't very elegant — the same variable (ls_day) appears multiple times in the code. And what if you need to compare a variable against 20 values instead of just 2?
 
-// Of course, you can use a pre-populated array:
+// LET'S CREATE WHAT IS MISSING
+
+// The proposed iin() function ("i"nternal "in") checks whether the value passed as the first argument exists in the array provided as the second argument:
+
+lb_weekend = iin(as_day, {'SATURDAY', 'SUNDAY'})
+
+// The fact that we can write array elements directly into the code using curly braces gives us a wide range of applications, and the code is easy to read.
+
+// Of course, you can pass a pre-populated array too. In this case, iin() becomes very similar to the contains() function that arrays have in some languages:
 
 string ls_days[] = {'SATURDAY', 'SUNDAY'}
 
-lb_weekend = iin(ls_day, ls_days[])
+lb_weekend = iin(as_day, ls_days[])
 
-// It's completely impossible to use an array with CHOOSE CASE—you’re forced to write an entire loop instead of a single line of code with iin()!
+// It's completely impossible to use an array with CHOOSE CASE — you’re forced to write an entire loop instead of one line of code with iin()!
 
 // Here’s another example, this time with numeric data:
 
