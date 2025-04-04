48a33ddaf9a487ffbee4803d40e67c7fe0bde52e
Ursego <90226705+Ursego@users.noreply.github.com>
2025-04-04 16:39:43 -0400

    Add files via upload

--- uf_ds_from_sql() - create DataStore from SELECT.cs
+++ uf_ds_from_sql() - create DataStore from SELECT.cs
@@ -6,6 +6,12 @@
 // One accepts a Transaction object through the a_tr argument.
 // Another overload doesn’t include that argument and uses SQLCA; it will likely be the most commonly used overload.
 
+// The function takes a boolean argument ab_retrieve.
+// If it's true, the function will not only create a DS but also populate it, so you can work with the retrieved data immediately after calling the function.
+// I recommend creating constants for this argument in the same class where you create the function:
+constant boolean RETRIEVE_DS = true
+constant boolean DONT_RETRIEVE_DS = false
+
 // More information and examples of use are provided in the header comment of the function.
 
 // ERRORS HANDLING
@@ -22,7 +28,7 @@
 Dscr:       Returns a DataStore created dynamically by the supplied SELECT statement.
 
             This func allows to AVOID:
-               1. Creation of a large number of DataObjects, used only once.
+               1. Flooding PBLs with single-use DataObjects whose visual representation is irrelevant.
                2. Complicated formats 3 and 4 of dynamic embedded SQL. It's much easier to build the SQL as a string,
                         pass it to uf_ds_from_sql() and then manipulate the retrieved data in the created DataStore.
 
@@ -30,7 +36,7 @@ Dscr:       Returns a DataStore created dynamically by the supplied SELECT state
             prior to passing it to uf_ds_from_sql(), for example:
 
             ls_sql = "SELECT " + as_list_of_fields_to_print + " FROM emp WHERE dept_id = " + String(al_dept_id)
-            lds_emp = gn_util.uf_ds_from_sql(ls_sql, true /* ab_retrieve */)
+            lds_emp = gn_util.uf_ds_from_sql(ls_sql)
 
             If an argument is an array, then you can create an IN clause using the function uf_in_clause_from_array():
             https://github.com/Ursego/PowerBuilder/blob/main/uf_in_clsause_from_array().cs
@@ -39,7 +45,7 @@ Dscr:       Returns a DataStore created dynamically by the supplied SELECT state
             ls_statuses_frag = gn_util.uf_in_clause_from_array("status", {"A", "N", "R"}) // returns "status IN ('A', 'N', 'R')"
             ls_depts_frag = gn_util.uf_in_clause_from_array("dept", {7, 11, 16}) // returns "dept IN (7, 11, 16)"
             ls_sql = "SELECT " + as_list_of_fields_to_print + " FROM emp WHERE " + ls_statuses_frag + " AND " + ls_depts_frag
-            lds_emp = gn_util.uf_ds_from_sql(ls_sql, true /* ab_retrieve */)
+            lds_emp = gn_util.uf_ds_from_sql(ls_sql)
 ***********************************************************************************************************************
 Arg:        as_sql: the SELECT statement to create the DS from (without ";").     
             ab_retrieve:
@@ -96,11 +102,18 @@ end if
 
 return lds
 
-// The overload which uses SQLCA; it has only 2 parameters - as_sql and ab_retrieve, but not a_tr:
+// The overload which uses SQLCA; it has only 2 arguments - as_sql and ab_retrieve, but not a_tr:
 
 return this.uf_ds_from_sql(as_sql, ab_retrieve, SQLCA)
 
+// The overload which uses SQLCA and retrieves; it has only 1 argument - as_sql, but not ab_retrieve and a_tr:
+
+return this.uf_ds_from_sql(as_sql, RETRIEVE_DS)
+
 // @@@ WITH exceptions:
+
+// Obviously, first of all you need to add the exceptions mechanism to your app:
+// https://github.com/Ursego/PowerBuilder/blob/main/Exceptions%20-%20the%20elegant%20way.cs
 
 // Don't forget to fill the "Throws:" field in the header with n_ex in both the overloads!
 
@@ -108,7 +121,7 @@ return this.uf_ds_from_sql(as_sql, ab_retrieve, SQLCA)
 Dscr:       Returns a DataStore created dynamically by the supplied SELECT statement.
 
             This func allows to AVOID:
-               1. Creation of a large number of DataObjects, used only once.
+               1. Flooding PBLs with single-use DataObjects whose visual representation is irrelevant.
                2. Complicated formats 3 and 4 of dynamic embedded SQL. It's much easier to build the SQL as a string,
                         pass it to uf_ds_from_sql() and then manipulate the retrieved data in the created DataStore.
 
@@ -116,7 +129,7 @@ Dscr:       Returns a DataStore created dynamically by the supplied SELECT state
             prior to passing it to uf_ds_from_sql(), for example:
 
             ls_sql = "SELECT " + as_list_of_fields_to_print + " FROM emp WHERE dept_id = " + String(al_dept_id)
-            lds_emp = gn_util.uf_ds_from_sql(ls_sql, true /* ab_retrieve */)
+            lds_emp = gn_util.uf_ds_from_sql(ls_sql)
 
             If an argument is an array, then you can create an IN clause using the function uf_in_clause_from_array():
             https://github.com/Ursego/PowerBuilder/blob/main/uf_in_clause_from_array().cs
@@ -125,7 +138,7 @@ Dscr:       Returns a DataStore created dynamically by the supplied SELECT state
             ls_statuses_frag = gn_util.uf_in_clause_from_array("status", {"A", "N", "R"}) // returns "status IN ('A', 'N', 'R')"
             ls_depts_frag = gn_util.uf_in_clause_from_array("dept", {7, 11, 16}) // returns "dept IN (7, 11, 16)"
             ls_sql = "SELECT " + as_list_of_fields_to_print + " FROM emp WHERE " + ls_statuses_frag + " AND " + ls_depts_frag
-            lds_emp = gn_util.uf_ds_from_sql(ls_sql, true /* ab_retrieve */)
+            lds_emp = gn_util.uf_ds_from_sql(ls_sql)
 ***********************************************************************************************************************
 Arg:        as_sql: the SELECT statement to create the DS from (without ";").     
             ab_retrieve:
@@ -133,9 +146,11 @@ Arg:        as_sql: the SELECT statement to create the DS from (without ";").
                   false = only create a DS, but don't retrieve.
             a_tr - Transaction object for created DS. To use SQLCA, call the overloaded version without this arg.
 ***********************************************************************************************************************
-Ret:         DataStore
+Ret:        DataStore
 ***********************************************************************************************************************
-Developer:   Michael Zuskin -  http://linkedin.com/in/zuskin | https://github.com/Ursego/
+Throws:     n_ex
+***********************************************************************************************************************
+Developer:  Michael Zuskin -  http://linkedin.com/in/zuskin | https://github.com/Ursego/
 **********************************************************************************************************************/
 long        ll_rc
 string      ls_err
@@ -172,6 +187,9 @@ end try
 
 return lds
 
-// The overload which uses SQLCA; it has only 2 parameters - as_sql and ab_retrieve, but not a_tr:
+// The overloads use the same code as the exceptionless overloads — just make them throw n_ex.
 
-return this.uf_ds_from_sql(as_sql, ab_retrieve, SQLCA)
+// Generally speaking, using SQL statements on the client side is not a good practice.
+// The client should call a stored procedure that encapsulates any existing (or potential) complexity and keeps it on the server side.  
+// But if you have no choice — for example, when maintaining an application where all the SQL is handled in PowerBuilder —
+//      then uf_ds_from_sql() avoids creation of a large number of DataObjects, used only once.
