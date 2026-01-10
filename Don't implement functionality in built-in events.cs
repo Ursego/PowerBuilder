cde338cb45867667bc3218dff4aae1a3b95bf524
Michael Zuskin <90226705+Ursego@users.noreply.github.com>
2026-01-10 09:44:29 -0500

    Update Don't implement functionality in built-in events.cs

--- Don't implement functionality in built-in events.cs
+++ Don't implement functionality in built-in events.cs
@@ -1,13 +1,13 @@
 // ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // If the functionality is longer than a few lines then don't implement it in system (built-in) events (such as ButtonClicked, ItemChanged etc.).
-// Instead, extract the logic into brand new well-named functions and call these functions from within the events.
+// Instead, extract the logic into brand new well-named functions and call them from the events.
 // ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // If a system event's script grows from just a few lines and starts to include its own logic, it effectively becomes "implementing functionality".
 // At that point, the logic should be extracted into a dedicated function (or dedicated functions).
 
 // You should divide your program into short methods, each performing a single, well-defined task (“separation of concerns”).  
-// So, if a built-in event needs to handle different logical tasks, implementing each task in its own function aligns with best coding practices.
+// So, if a built-in event needs to handle different logical tasks, implementing each one in its own function as per best coding practices.
 
 // In fact, some built-in events are really just traffic hubs for routing program flow to the appropriate functions using a choose case structure.
 
@@ -20,7 +20,7 @@ case "cb_print_summary"
     this.uf_print_summary(row)
 end choose
 
-// This approach makes it easier to focus on what happens when the user clicks one particular button, without being distracted by unrelated processing in the same script.
+// This approach makes it easier to focus on what happens when the user clicks the button, without being distracted by unrelated processing in the same script.
 
 // Also, consider this: one day, the event's functionality might need to be called from somewhere else.  
 // You should avoid calling built-in GUI events directly — unless you're using 'call super' from a descendant.  
@@ -29,9 +29,9 @@ end choose
 
 // @@@ ItemChanged
 
-// Sometimes, the processing of a field in ItemChanged includes a few short actions unrelated to each other.
-// Creating a function for each such action would be an overkill.
-// So, I extract the fragment for that field into one function, and name it using the pattern uf_<built-in event>__<column name> (note the two underscores).
+// Sometimes, the processing of one particular field in ItemChanged includes a few short actions unrelated to each other.
+// Since they are short, creating a function for each such action would be an overkill.
+// So, I extract the fragment for that specific field into one function, and name it using the pattern uf_<built-in event>__<column name> (note the two the underscores).
 // For example: uf_itemchanged__birth_date, uf_itemchanged__country_id.
 
 // Each such a function must return the same values which ItemChanged returns:
@@ -66,5 +66,6 @@ if li_rc <> 0 then return li_rc
 
 return 0
 
-// It’s perfectly reasonable to end up with many uf_itemchanged__XXX functions - this happened to me a lot of times.
+// It’s perfectly reasonable to end up with many uf_itemchanged__XXX functions - it has happened to me many times.
+
 
